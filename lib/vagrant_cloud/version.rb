670fd358378a29f6cd6e4e4c6374c590a28e70f3
Reto Kaiser <reto@cargomedia.ch>
2015-07-01 16:51:41 +0200

    Add docu

--- lib/vagrant_cloud/version.rb
+++ lib/vagrant_cloud/version.rb
@@ -6,36 +6,46 @@ module VagrantCloud
     attr_accessor :number
     attr_accessor :data
 
+    # @param [Box] box
+    # @param [String] number
+    # @param [Hash] data
     def initialize(box, number, data = nil)
       @box = box
       @number = number
       @data = data
     end
 
+    # @return [String]
     def version
       data['version'].to_s
     end
 
+    # @return [String]
     def description
       data['description_markdown'].to_s
     end
 
+    # @return [String]
     def status
       data['status'].to_s
     end
 
+    # @return [Array<Provider>]
     def providers
       data['providers'].map { |data| Provider.new(self, data['name'], data) }
     end
 
+    # @return [String]
     def to_s
       version
     end
 
+    # @return [Hash]
     def data
       @data ||= account.request('get', "/box/#{account.username}/#{box.name}/version/#{number}")
     end
 
+    # @param [String] description
     def update(description)
       version = {:description => description}
       @data = account.request('put', "/box/#{account.username}/#{box.name}/version/#{number}", {:version => version})
@@ -53,16 +63,25 @@ module VagrantCloud
       @data = account.request('put', "/box/#{account.username}/#{box.name}/version/#{number}/revoke")
     end
 
+    # @param [String] name
+    # @param [Hash] data
+    # @return [Provider]
     def get_provider(name, data = nil)
       Provider.new(self, name, data)
     end
 
+    # @param [String] name
+    # @param [String] url
+    # @return [Provider]
     def create_provider(name, url)
       params = {:name => name, :url => url}
       data = account.request('post', "/box/#{account.username}/#{box.name}/version/#{self.number}/providers", {:provider => params})
       get_provider(name, data)
     end
 
+    # @param [String] name
+    # @param [String] url
+    # @return [Provider]
     def ensure_provider(name, url)
       provider = providers.select{ |provider| provider.name == name }.first
       unless provider
@@ -76,6 +95,7 @@ module VagrantCloud
 
     private
 
+    # @return [Account]
     def account
       box.account
     end
