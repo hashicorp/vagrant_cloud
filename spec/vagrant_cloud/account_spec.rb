c3dc1c3d8e42a167445a13a519529c598a9f1fb0
Reto Kaiser <reto@cargomedia.ch>
2015-07-01 10:59:47 +0200

    formatting

--- spec/vagrant_cloud/account_spec.rb
+++ spec/vagrant_cloud/account_spec.rb
@@ -5,7 +5,6 @@ describe VagrantCloud::Account do
   let (:account) { VagrantCloud::Account.new('my-username', 'my-token') }
 
   describe '#initialize' do
-
     it 'stores credentials' do
       expect(account.username).to eq('my-username')
       expect(account.access_token).to eq('my-token')
