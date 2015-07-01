c79b092ab6a2c5bdeb6f84ae03c66a92410ee810
Reto Kaiser <reto@cargomedia.ch>
2015-07-01 11:52:49 +0200

    Add some account specs

--- lib/vagrant_cloud/account.rb
+++ lib/vagrant_cloud/account.rb
@@ -52,7 +52,7 @@ module VagrantCloud
       )
       result = JSON.parse(result)
       errors = result['errors']
-      raise "Vagrant Cloud returned error: #{errors}" if errors
+      raise(RuntimeError, "Vagrant Cloud returned error: #{errors}") if errors
       result
     end
 
--- spec/vagrant_cloud/account_spec.rb
+++ spec/vagrant_cloud/account_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'vagrant_cloud'
 
 describe VagrantCloud::Account do
@@ -8,6 +9,64 @@ describe VagrantCloud::Account do
     it 'stores credentials' do
       expect(account.username).to eq('my-username')
       expect(account.access_token).to eq('my-token')
+    end
+  end
+
+  describe '.request' do
+    it 'sends post params' do
+      stub_request(:post, 'https://vagrantcloud.com/api/v1/foo').with(
+        :body => {
+          :access_token => 'my-token',
+          :foo => 'foo',
+        },
+        :headers => {
+          :access_token => 'my-token',
+        }
+      ).to_return(status: 200, body: JSON.dump({:bar => 'bar'}))
+
+      expect(account.request(:post, '/foo', {:foo => 'foo'})).to eq({'bar' => 'bar'})
+    end
+
+    it 'sends get params' do
+      stub_request(:get, 'https://vagrantcloud.com/api/v1/foo').with(
+        :body => {
+          :access_token => 'my-token',
+          :foo => 'foo',
+        },
+        :headers => {
+          :access_token => 'my-token',
+        }
+      ).to_return(status: 200, body: JSON.dump({:bar => 'bar'}))
+
+      expect(account.request(:get, '/foo', {:foo => 'foo'})).to eq({'bar' => 'bar'})
+    end
+
+    it 'raises on error' do
+      stub_request(:get, 'https://vagrantcloud.com/api/v1/foo').
+        to_return(status: 200, body: JSON.dump({:errors => 'bar'}))
+
+      expect { account.request(:get, '/foo', {:foo => 'foo'}) }.
+        to raise_error(RuntimeError, 'Vagrant Cloud returned error: bar')
+    end
+  end
+
+  describe '.create_box' do
+    it 'creates and returns box' do
+      result = {'return_foo' => 'foo'}
+      stub_request(:post, 'https://vagrantcloud.com/api/v1/boxes').with(:body =>
+          {
+            :access_token => 'my-token',
+            :box => {
+              :name => 'my-name',
+              :description => 'my-desc',
+              :short_description => 'my-desc',
+              :is_private => '1',
+            }
+          }).to_return(status: 200, body: JSON.dump(result))
+
+      box = account.create_box('my-name', 'my-desc', true)
+      expect(box).to be_a(VagrantCloud::Box)
+      expect(box.data).to eq(result)
     end
   end
 
