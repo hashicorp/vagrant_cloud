a3a407dc4ad72940ef7ea373653861044f1503cd
Reto Kaiser <reto@cargomedia.ch>
2015-07-01 10:55:03 +0200

    Add spec skeleton

---
+++ spec/vagrant_cloud/account_spec.rb
@@ -0,0 +1,15 @@
+require 'vagrant_cloud'
+
+describe VagrantCloud::Account do
+
+  let (:account) { VagrantCloud::Account.new('my-username', 'my-token') }
+
+  describe '#initialize' do
+
+    it 'stores credentials' do
+      expect(account.username).to eq('my-username')
+      expect(account.access_token).to eq('my-token')
+    end
+  end
+
+end
--- vagrant_cloud.gemspec
+++ vagrant_cloud.gemspec
@@ -12,4 +12,6 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'rest-client', '~>1.7'
 
   s.add_development_dependency 'rake', '~>10.4'
+  s.add_development_dependency 'rspec', '~> 2.0'
+  s.add_development_dependency 'webmock', '~> 1.21'
 end
