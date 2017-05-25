110f9e5231a5786f59b6dfc92b4aac51b018800a
andrewconrad21 <andrewconrad.spam@gmail.com>
2017-05-25 04:27:12 -0500

    removed unnecessary method; cleaned up documentation

--- lib/vagrant_cloud/cli.rb
+++ lib/vagrant_cloud/cli.rb
@@ -29,7 +29,7 @@ module VagrantCloud
     desc 'create_provider', 'creates a provider within a given box and version'
     method_option :version, alias: '-v', required: true, desc: 'version within the box'
     method_option :provider, alias: '-p', default: 'virtualbox', desc: 'the provider for the box; default: virtualbox'
-    method_option :provider_url, alias: '-pu', desc: 'URL to the file for remote hosting; either a _url or _file_path can be provided, not both'
+    method_option :provider_url, alias: '-pu', desc: 'URL to the file for remote hosting; do not include if you intend to upload a file subsequently'
     def create_provider
       provider = current_version.create_provider(options[:provider], options[:provider_url])
       puts "created #{provider.data['name']} provider within version #{provider.version.version}" if options[:verbose]
@@ -60,12 +60,6 @@ module VagrantCloud
 
     def get_provider(provider_str)
       current_version.get_provider(provider_str)
-    end
-
-    def bump_version(version_str)
-      parts = version_str.split('.')
-      parts.push(parts.pop.to_i + 1)
-      parts.join('.')
     end
   end
 end
