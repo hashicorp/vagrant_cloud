1c4ab1d8e38538ef08391d13083d0aaa33dad729
Brian Cain <bcain@hashicorp.com>
2018-09-19 14:58:34 -0700

    Update based on rubocop offenses

--- lib/vagrant_cloud/box.rb
+++ lib/vagrant_cloud/box.rb
@@ -10,7 +10,7 @@ module VagrantCloud
     # @param [String] description
     # @param [String] short_description
     # @param [String] access_token
-    def initialize(account, name=nil, data = nil, short_description=nil, description=nil, access_token = nil, custom_server=nil)
+    def initialize(account, name = nil, data = nil, short_description = nil, description = nil, access_token = nil, custom_server = nil)
       @account = account
       @name = name
       @data = data
@@ -52,7 +52,7 @@ module VagrantCloud
     # @param [String] org - organization of the box to read
     # @param [String] box_name - name of the box to read
     # @return [Hash]
-    def delete(org=nil, box_name=nil)
+    def delete(org = nil, box_name = nil)
       @client.request('delete', box_path(org, box_name))
     end
 
@@ -61,7 +61,7 @@ module VagrantCloud
     # @param [String] org - organization of the box to read
     # @param [String] box_name - name of the box to read
     # @return [Hash]
-    def read(org=nil, box_name=nil)
+    def read(org = nil, box_name = nil)
       @client.request('get', box_path(org, box_name))
     end
 
@@ -69,7 +69,7 @@ module VagrantCloud
     # @param [String] description
     # @param [Bool] is_private
     # @return [Hash]
-    def create(short_description=nil, description=nil, org=nil, box_name=nil, is_private=false)
+    def create(short_description = nil, description = nil, org = nil, box_name = nil, is_private = false)
       update_data = !(org && box_name)
 
       org ||= account.username
@@ -82,7 +82,7 @@ module VagrantCloud
         username: org,
         is_private: is_private,
         short_description: short_description,
-        description: description,
+        description: description
       }.delete_if { |_, v| v.nil? }
 
       data = @client.request('post', '/boxes', box: params)
@@ -168,7 +168,7 @@ module VagrantCloud
     # @param [String] - username
     # @param [String] - box_name
     # @return [String] - API path to box
-    def box_path(username=nil, box_name=nil)
+    def box_path(username = nil, box_name = nil)
       if username && box_name
         "/box/#{username}/#{box_name}"
       else
