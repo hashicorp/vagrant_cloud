b0f00a0a7b39797def2c2222df3f42f231af289c
Chris Roberts <croberts@hashicorp.com>
2020-10-27 15:23:22 -0700

    Add notifications when running release flow

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export SLACK_USERNAME="Vagrant Cloud RubyGem"
+export SLACK_ICON="https://avatars.slack-edge.com/2017-10-17/257000837696_070f98107cdacc0486f6_36.png"
+export SLACK_TITLE="💎 RubyGems Publishing"
+
 csource="${BASH_SOURCE[0]}"
 while [ -h "$csource" ] ; do csource="$(readlink "$csource")"; done
 root="$( cd -P "$( dirname "$csource" )/../" && pwd )"
@@ -8,5 +12,9 @@ root="$( cd -P "$( dirname "$csource" )/../" && pwd )"
 
 pushd "${root}" > "${output}"
 
+slack "📢 New vagrant_cloud release has been triggered"
+
 # Build and publish our gem
 publish_to_rubygems
+
+slack -m "New version of vagrant_cloud published: v${version}"
